59a7b71bdbfc76117b360418a0be3e58334fa31b
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-20 01:19:50 -0400

    Add search bar that filters titles

--- marvel-explorer/src/components/List.jsx
+++ marvel-explorer/src/components/List.jsx
@@ -1,77 +1,97 @@
 import { useState, useEffect } from "react";
 
 const List = ({ publicKey }) => {
-  const [newReleases, setNewReleases] = useState([]);
+    const [newReleases, setNewReleases] = useState([]);
+    const [searchString, setSearchString] = useState("");
   
-  useEffect(() => {
-    const fetchNewReleases = async () => {
-        try {
-            const oneMonthAgo = new Date();
-            oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
-            const formattedDate = oneMonthAgo.toISOString();
-            
-            // Fetch new releases with a modified date since one month ago
-            const ts = '1';
-            // const hash = md5(ts + secretKey + publicKey);
-            const hash = '4637f3638671ce9485ecb2f0d3211bb5';
-            const response = await fetch(
-                `https://gateway.marvel.com/v1/public/comics?modifiedSince=${formattedDate}&apikey=${publicKey}&ts=${ts}&hash=${hash}`
-                );
-            const data = await response.json();
-
-            // Check if data contains new releases
-            if (data && data.data && data.data.results && data.data.results.length > 0) {
-                const allNewReleases = data.data.results;
-
-                // Sort new releases in descending order based on the modified date
-                allNewReleases.sort((a, b) => new Date(b.modified) - new Date(a.modified));
+    useEffect(() => {
+        const fetchNewReleases = async () => {
+            try {
+                const oneMonthAgo = new Date();
+                oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+                const formattedDate = oneMonthAgo.toISOString();
                 
-                setNewReleases(allNewReleases);
+                // Fetch new releases with a modified date since one month ago
+                const ts = '1';
+                // const hash = md5(ts + secretKey + publicKey);
+                const hash = '4637f3638671ce9485ecb2f0d3211bb5';
+                const response = await fetch(
+                    `https://gateway.marvel.com/v1/public/comics?modifiedSince=${formattedDate}&apikey=${publicKey}&ts=${ts}&hash=${hash}`
+                    );
+                const data = await response.json();
+
+                // Check if data contains new releases
+                if (data && data.data && data.data.results && data.data.results.length > 0) {
+                    const allNewReleases = data.data.results;
+
+                    // Sort new releases in descending order based on the modified date
+                    allNewReleases.sort((a, b) => new Date(b.modified) - new Date(a.modified));
+                    
+                    setNewReleases(allNewReleases);
+                }
+            } catch (error) {
+                console.error('Error:', error);
             }
-        } catch (error) {
-            console.error('Error:', error);
-        }
+        };
+        
+        fetchNewReleases();
+    }, [publicKey]);
+
+    const searchItems = (inputString) => {
+        setSearchString(inputString);
     };
-    
-    fetchNewReleases();
-  }, []);
+
+    const filteredReleases = newReleases.filter((release) =>
+        release.title.toLowerCase().includes(searchString.toLowerCase())
+    );
+
 
   return (
-    <div className="max-h-72 overflow-y-auto">
-      <table className="my-3 w-full ">
-        <thead className="border border-gray-300 bg-gray-200 text-black sm:text-lg">
-            <tr>
-                <th className="border-gray-400 border p-2">Image</th>
-                <th className="border-gray-400 border p-2">Title</th>
-                <th className="border-gray-400 border p-2">Release Date</th>
-            </tr>
-        </thead>
-        <tbody className="border border-gray-200 text-black bg-white">
-            {newReleases.map((release, index) => (
-                <tr key={release.id}>
-                    <td className="border border-gray-200 overflow-clip flex justify-center p-1">
-                        <img
-                            src={`${release.thumbnail.path}.${release.thumbnail.extension}`}
-                            alt={release.title}
-                            className="h-14 rounded shadow-sm shadow-black flex"
-                        />
-                    </td>
-                    <td className="border border-gray-200 overflow-clip text-sm font-medium">
-                        {release.title}
-                    </td>
-                    <td className="border border-gray-200 overflow-clip text-sm italic">
-                        {new Date(release.modified).toLocaleDateString(undefined, {
-                            weekday: 'long',
-                            year: 'numeric',
-                            month: 'long',
-                            day: 'numeric',
-                        })}
-                    </td>
-                </tr>
-            ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+        <div className="flex justify-center">
+            <input
+                type="text"
+                placeholder="Search new titles"
+                className="w-1/3 font-medium italic py-1 px-3 rounded-md text-sm shadow-sm shadow-gray-900/20"
+                onChange={(e) => searchItems(e.target.value)}
+            />
+        </div>
+        <div classNakme="max-h-72 overflow-y-auto">
+            <table className="my-3 w-full ">
+                <thead className="border border-gray-300 bg-gray-200 text-black sm:text-lg">
+                    <tr>
+                        <th className="border-gray-400 border p-2">Image</th>
+                        <th className="border-gray-400 border p-2">Title</th>
+                        <th className="border-gray-400 border p-2">Release Date</th>
+                    </tr>
+                </thead>
+                <tbody className="border border-gray-200 text-black bg-white">
+                    {filteredReleases.map((release, index) => (
+                        <tr key={release.id}>
+                            <td className="border border-gray-200 overflow-clip flex justify-center p-1">
+                                <img
+                                    src={`${release.thumbnail.path}.${release.thumbnail.extension}`}
+                                    alt={release.title}
+                                    className="h-14 rounded shadow-sm shadow-black flex"
+                                />
+                            </td>
+                            <td className="border border-gray-200 overflow-clip text-sm font-medium">
+                                {release.title}
+                            </td>
+                            <td className="border border-gray-200 overflow-clip text-sm italic">
+                                {new Date(release.modified).toLocaleDateString(undefined, {
+                                    weekday: 'long',
+                                    year: 'numeric',
+                                    month: 'long',
+                                    day: 'numeric',
+                                })}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    </>
   );
 }
 
